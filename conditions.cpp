bba3f31a65f70b536c8b31750db3abdeb4d95f58
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-10 11:41:38 +0000

    conditons

---
+++ conditions.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n;
+    // cin>>n;
+    // cout<<"Value of n is "<< n <<endl;
+    // if(n>0){
+    //     cout<<"n is positive"<< endl;
+    // }
+    // else{
+    //     cout<<"n is negative"<< endl;
+    // }
+
+
+    int a, b;
+    cin>> a >> b;
+    cout<< "Value of a and b is "<<a<<" "<<b<<endl;
+}
