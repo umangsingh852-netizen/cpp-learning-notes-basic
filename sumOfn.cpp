b68ba9dd158332307d996476de25be0b6475add7
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-10 12:02:48 +0000

    sum of n question

---
+++ sumOfn.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int sum = 0;
+    int i = 1;
+    while(i<= n){
+        sum = sum+i;
+        i = i + 1;
+    }
+    cout<<"The sum of given n number is "<<sum<<endl;
+}
